d7fe19c627e530afb1ea16552a32d218ee84dc53
KoheiMatsuura7 <kasutera7@gmail.com>
2025-06-14 20:30:25 +0900

    Add files via upload

---
+++ card_data.js
@@ -0,0 +1,34 @@
+// card_data.js
+const allCards = [
+    {
+        name: "リーリエ(SM1M-066)",
+        category: "ポケカ",
+        subcategory: "SR",
+        price: 200000,
+        imgSrc: "https://cdn.peraichi.com/userData/577d7af7-4e20-4459-b45d-21ac0a00005c/img/910512b0-2a6d-013e-45f2-0a58a9feac02/035622_T_RIRIE.jpg"
+    },
+    {
+        name: "ポンチョを着たピカチュウ(207/XY-P)",
+        category: "ポケカ",
+        subcategory: "プロモ",
+        price: 3000,
+        imgSrc: "https://cdn.peraichi.com/userData/577d7af7-4e20-4459-b45d-21ac0a00005c/img/1bf5b5a0-2a6a-013e-4579-0a58a9feac02/031640_P_PONCHOWOKITAPIKACHUU.jpg"
+    },
+    {
+        name: "ルフィ",
+        category: "ワンピカード",
+        subcategory: "SAR",
+        price: 900,
+        imgSrc: "https://i.imgur.com/g0wWnA4.jpg"
+    }
+    // ここに他のカード情報を追加していく
+    // 例:
+    // ,
+    // {
+    //     name: "新しいカード名",
+    //     category: "カテゴリ名",
+    //     subcategory: "サブカテゴリ名",
+    //     price: 1500,
+    //     imgSrc: "https://example.com/new_card.jpg"
+    // }
+];
