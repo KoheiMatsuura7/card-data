b64f48397545f48e6861d738054f958b6d4075b3
KoheiMatsuura7 <kasutera7@gmail.com>
2025-06-23 00:58:33 +0900

    Add files via upload

--- card_data.js
+++ card_data.js
@@ -595,13 +595,6 @@ const allCards = [
         "imgSrc": "https://cdn.peraichi.com/userData/577d7af7-4e20-4459-b45d-21ac0a00005c/img/7841c180-2b31-013e-8430-0a58a9feac02/Black%20Magician.png"
     },
     {
-        "name": "変化の仮面",
-        "category": "ポケカ",
-        "subcategory": "未開封BOX",
-        "price": 7000,
-        "imgSrc": "https://cdn.peraichi.com/userData/577d7af7-4e20-4459-b45d-21ac0a00005c/img/3f8383f0-2c0a-013e-f186-0a58a9feac02/71SyXDvePnL._UF894,1000_QL80_.jpg"
-    },
-    {
         "name": "ラティアス&ラティオス【SA】(105/095)PSA",
         "category": "ポケカ",
         "subcategory": "PSA鑑定品",
